763873ad627161e9b4cab8ab21d61d121a3d84f6
rakibhossainraju <rakibhossianraju@gamil.com>
2025-12-30 00:04:59 +0600

    feat(text-hover): enhance character animation on hover with GSAP and improve CSS structure

--- examples/text-hover/index.ts
+++ examples/text-hover/index.ts
@@ -1,31 +1,66 @@
 import { gsap } from 'gsap';
 import { SplitText } from 'gsap/SplitText';
-
+import { GSDevTools } from 'gsap/GSDevTools';
+gsap.registerPlugin(SplitText, GSDevTools);
+const CHAR_GAP = 50;
 const headings = document.querySelectorAll<HTMLHeadingElement>('.text');
 
 headings.forEach((heading) => {
-  const splittedText = new SplitText(heading, {
+  new SplitText(heading, {
     type: 'chars',
     autoSplit: true,
     charsClass: 'char-container',
-    onSplit: animateText,
+    onSplit: deepSplitChar,
   });
 });
 
-function animateText(splittedText: SplitText) {
+function deepSplitChar(splittedText: SplitText) {
+  const topChars: HTMLDivElement[] = [];
+  const bottomChars: HTMLDivElement[] = [];
+
   splittedText.chars.forEach((char, i) => {
     const textContent = char.textContent || '';
 
     const topDiv = document.createElement('div');
-    topDiv.classList.add('char-top');
+    topDiv.classList.add('char', 'char-top');
     topDiv.textContent = textContent;
 
     const bottomDiv = document.createElement('div');
-    bottomDiv.classList.add('char-bottom');
+    bottomDiv.classList.add('char', 'char-bottom');
+    bottomDiv.style.setProperty('--char-gap', `${CHAR_GAP}px`);
     bottomDiv.textContent = textContent;
 
     char.textContent = '';
     char.appendChild(topDiv);
     char.appendChild(bottomDiv);
+    topChars.push(topDiv);
+    bottomChars.push(bottomDiv);
+  });
+
+  headings.forEach((heading) => {
+    animateChars(topChars, bottomChars, heading);
   });
 }
+function animateChars(
+  topChars: HTMLDivElement[],
+  bottomChars: HTMLDivElement[],
+  heading: HTMLHeadingElement
+) {
+  // Create a separate timeline for each heading
+  const defaults = { duration: 0.6, stagger: 0.02, ease: 'power3.out', overwrite: true };
+
+  const elementHeight = topChars[0].offsetHeight + CHAR_GAP;
+  gsap.set(bottomChars, { y: elementHeight, opacity: 1 });
+
+  heading.addEventListener('mouseenter', () => {
+    gsap.to(topChars, { yPercent: -100, ...defaults });
+    gsap.to(bottomChars, { y: 0, ...defaults });
+  });
+
+  heading.addEventListener('mouseleave', () => {
+    gsap.to(topChars, { yPercent: 0, ...defaults });
+    gsap.to(bottomChars, { y: elementHeight, ...defaults });
+  });
+}
+
+function onHoverChar(event: MouseEvent) {}
